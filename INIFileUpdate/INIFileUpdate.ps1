19edad8a724b71ec1181c6437d3732d2024b33d6
Bozhidar Bekriev <BBe@milestone.dk>
2019-11-15 16:37:18 +0200

    minor updates

--- INIFileUpdate/INIFileUpdate.ps1
+++ INIFileUpdate/INIFileUpdate.ps1
@@ -2,7 +2,7 @@
 .SYNOPSIS
 Overwrites specific sections in a given INI file
 .DESCRIPTION
-This script takes input from TeamCity step (meta-runner) parameters and updates specific values from an INI configuration file. 
+This script takes input from TeamCity step (meta-runner) parameters and updates specific values from an INI configuration file.
 When calling the script, use either 'ByValue' or 'ByPath' parameter set by specifying the respective paramter depending on if you are updating file path values or short values (boolean, int, etc.)
 Meant to be called in two subsequent build steps/meta-runners if it's required to update both paths and values
 #>
@@ -10,60 +10,56 @@ Meant to be called in two subsequent build steps/meta-runners if it's required t
 [CmdletBinding()]
 param (
     #Path to input INI file.
-    [Parameter(Mandatory=$TRUE,Position=0)]
+    [Parameter(Mandatory = $TRUE, Position = 0)]
     [string]$InputFilePath,
 
     #Path to output INI file
-    [Parameter(Mandatory=$TRUE,Position=1)]
+    [Parameter(Mandatory = $TRUE, Position = 1)]
     [string]$OutputFilePath,
 
     #'||' delimited triplets of INI configuration VALUES items:Section;Key;Value||Section1;Key1;Value1
     # ex.: Application;DOUBLE-BYTE;TRUE||Revert Settings;REVERT_VAL_ERROR;FALSE||Online Services;RegexValidationServer;http://someURL
-    [Parameter(Mandatory=$TRUE,ParameterSetName="ByValue")]
+    [Parameter(Mandatory = $TRUE, ParameterSetName = "ByValue")]
     [string]$ValuesToUpdate,
 
-    #'||' delimited triplets of INI configuration PATHS items:Section;Key;Path||Section1;Key1;Path1
-    [Parameter(Mandatory=$TRUE,ParameterSetName="ByPath")]
-    [string]$PathsToUpdate,
+    # Path to file containing CSV values (Section;Key;Value) split by GroupDelimiter
+    [Parameter(Mandatory = $TRUE, ParameterSetName = "ByValuesFile")]
+    [string]$InputValuesFile,
 
     #Default section delimiter is "||" but other can be specified if escaping necessary
-    [Parameter(Mandatory=$FALSE)]
+    [Parameter(Mandatory = $FALSE)]
     [string]$SectionDelimiter = "||",
 
     #Default group delimiter is ';' but other can be specified if escaping necessary
-    [Parameter(Mandatory=$FALSE)]
+    [Parameter(Mandatory = $FALSE)]
     [string]$GroupDelimiter = ';'
-    )
+)
 
 TRY {
     $ErrorActionPreference = "Stop"
 
-    Import-Module "$PSScriptRoot\internal\psini\PsIni.psm1"
+    $inputs = $null;
 
-    $INIConfiguration = Get-IniContent $InputFilePath
-
-    IF ($PSCmdlet.ParameterSetName -eq 'ByValue') 
-    {	
-        Write-Host "`nThe following values were updated:`n_________________________________`n"
-        $ValuesToUpdate.Split($SectionDelimiter) | ConvertFrom-Csv -Delimiter $GroupDelimiter  -Header "Section", "Key", "Value" | ForEach-Object {
-            $INIConfiguration[$_.Section][$_.Key] = $_.Value
-            $_
-        }
-    } 
-    ELSE 
-    {
-        Write-Host "`nThe following paths were updated:`n_________________________________`n"
-        $PathsToUpdate.Split($SectionDelimiter) | ConvertFrom-Csv -Delimiter $GroupDelimiter  -Header "Section", "Key", "Value" | ForEach-Object {
-            $INIConfiguration[$_.Section][$_.Key] = $_.Value
-            $_
-        }
+    IF ($PSCmdlet.ParameterSetName -eq 'ByValue') {
+        $inputs = $ValuesToUpdate.Split($SectionDelimiter) | ConvertFrom-Csv -Delimiter $GroupDelimiter  -Header "Section", "Key", "Value"
+    } ELSE {
+        $inputs = Import-Csv -LiteralPath $InputValuesFile -Delimiter $GroupDelimiter  -Header "Section", "Key", "Value"
     }
 
-    "`nOutput to: $OutputFilePath ..."
+    Import-Module "$PSScriptRoot\internal\psini\PsIni.psm1"
+    $INIConfiguration = Get-IniContent $InputFilePath
+
+    Write-Host "The following values were updated:"
+    Write-Host "_________________________________"
+    $inputs | ForEach-Object {
+        $INIConfiguration[$_.Section][$_.Key] = $_.Value
+        Write-Host $_
+    }
+
+    Write-Host "`nOutput to: $OutputFilePath ..."
 
     $INIConfiguration | Out-IniFile $OutputFilePath -Force
-}
-CATCH {
+} CATCH {
     Write-Host "Failed to update INI file..."
     Write-Host $_
     exit 42
